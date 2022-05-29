f185d3ecc171ea62e6b9eb3b991bed8add82f1bd
Windo Hutabarat <whutabarat@gmail.com>
2022-05-29 23:55:33 +0100

    Initial ITGrade class and function

--- src/lgtoldes/itgrade.py
+++ src/lgtoldes/itgrade.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-def print_something(something):
-    print(something, flush=True)
-    return something
+import pandas as pd
+from importlib import resources
 
 def getStdTolVal(normSize, ITGrd, ITGrdDF):
     StdTolVal = ITGrdDF[(normSize > ITGrdDF.lowerBound) & (normSize <= ITGrdDF.upperBound)].loc[:, ITGrd]
@@ -11,4 +10,9 @@ def getStdTolVal(normSize, ITGrd, ITGrdDF):
 
 class ITGrd:
     def __init__(self):
-        self.ITGrdTblZZ
+        with resources.path("lgtoldes.data", "ITGradeTable.csv") as f_tbl_path:
+            self.ITGrdDF = pd.read_csv(f_tbl_path)
+    
+    def getStdTolVal(self, normSize, ITGrd):
+        StdTolVal = self.ITGrdDF[(normSize > self.ITGrdDF.lowerBound) & (normSize <= self.ITGrdDF.upperBound)].loc[:, ITGrd]
+        return float(StdTolVal)
