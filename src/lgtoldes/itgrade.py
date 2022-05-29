f90f2a5e4a840752ad676be4867d1977f5447dba
Windo Hutabarat <whutabarat@gmail.com>
2022-05-29 23:10:47 +0100

    Reorganised project structure

--- src/lgtoldes/itgrade.py
+++ src/lgtoldes/itgrade.py
@@ -4,3 +4,11 @@
 def print_something(something):
     print(something, flush=True)
     return something
+
+def getStdTolVal(normSize, ITGrd, ITGrdDF):
+    StdTolVal = ITGrdDF[(normSize > ITGrdDF.lowerBound) & (normSize <= ITGrdDF.upperBound)].loc[:, ITGrd]
+    return float(StdTolVal)
+
+class ITGrd:
+    def __init__(self):
+        self.ITGrdTblZZ
